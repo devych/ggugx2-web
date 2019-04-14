11ef4a2ebd7909c6ed9ae4fa3d65ea3533815534
devych <ych900809@gmail.com>
2019-04-15 00:25:27 +0900

    add organisms

---
+++ src/components/organisms/contentsList/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ContentBox from '../../molecules/contentBox/index';
+
+const contentsList = () => {
+  return <ContentBox contentTitle={'showContent'} />;
+};
+
+export default contentsList;
---
+++ src/components/organisms/infoSetting/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SubNavBar from '../../molecules/subNavBar/index';
+import ImageBox from '../../molecules/imageBox/index';
+
+const InfoSetting = () => {
+  return <div />;
+};
+
+export default InfoSetting;
