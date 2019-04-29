b8dad7ebdc7c6802ab8028cc431bdb237bcf8a72
devych <ych900809@gmail.com>
2019-04-29 11:21:56 +0900

    for mc

--- src/components/atoms/nav/index.js
+++ src/components/atoms/nav/index.js
@@ -2,25 +2,29 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './index.css';
 
-const Nav = () => {
+const Nav = ({ storeName }) => {
   return (
-    <span className="nav">
-      <NavLink to="/MainPage" className="item">
+    <span>
+      <span className="nav">
+        <span className="item" id="storeName">
+          {storeName} 정보
+        </span>
+        {/* <NavLink to="/MainPage" className="item">
         Main
-      </NavLink>
-      <NavLink to="/StampsRewards" className="item">
-        적립관리
-      </NavLink>
-      <NavLink to="/CustomerMng" className="item">
+      </NavLink> */}
+        <NavLink to="/StampsRewards" className="item">
+          적립관리
+        </NavLink>
+        {/* <NavLink to="/CustomerMng" className="item">
         고객관리
-      </NavLink>
-      <NavLink to="/ShopMng" className="item">
-        매장관리
-      </NavLink>
-      <NavLink to="/Statistic" className="item">
+      </NavLink> */}
+        <NavLink to="/ShopMng" className="item">
+          매장관리
+        </NavLink>
+        {/* <NavLink to="/Statistic" className="item">
         통계
-      </NavLink>
-      {sessionStorage.getItem('token') ? (
+      </NavLink> */}
+        {/* {sessionStorage.getItem('token') ? (
         <NavLink to="/Signin" className="item" style={{ display: 'none' }}>
           Signin
         </NavLink>
@@ -28,7 +32,8 @@ const Nav = () => {
         <NavLink to="/Signin" className="item">
           Signin
         </NavLink>
-      )}
+      )} */}
+      </span>
     </span>
   );
 };
